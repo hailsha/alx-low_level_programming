d1b7718982ea888b2c2fba559aa33271415e644d
hailemariam <hailemariam37@gmail.com>
2022-04-12 05:36:05 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,40 +9,41 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	if( n>=0 && n<= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				int product = i * j;
+	int row;
+	int column;
+	int product;
 
-				if (j == 0)
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = (row * column);
+				if (column == 0)
 					_putchar('0' + product);
-				else if (j <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (product % 10));
-				}
-				else if (product <= 99)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
 				else
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				if (j < n)
 				{
 					_putchar(',');
 					_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
 				}
 			}
 			_putchar('\n');
