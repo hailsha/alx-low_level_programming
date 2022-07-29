0bb55b582d9fce875afb37eaba085527c6827b5b
hailemariam <hailemariam37@gmail.com>
2022-07-29 01:50:03 -0700

    Fixing the error

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,16 +1,16 @@
-include "hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * key_index - function that generates a distributed index
- * according to a given hash
- *
- * @key: key passed
- * @size: size of the hash tables
- * Return: key index
+ * key_index - Function that locates and returns the index
+ * of a key. The hash index is taken by dividing our integer
+ * obtained by using the djb2 function by the size of the hash
+ * table, and obtaining the remainder. This is also known as
+ * the remainder method
+ * @key: The key. A passed in string
+ * @size: Size of the array
+ * Return: The index where the key should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == 0)
-		return (0);
-	return (hash_djb2(key) % size);
+	return ((hash_djb2(key)) % size);
 }
